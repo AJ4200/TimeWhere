2c1e45a6aeab337345470eab0db20c5b40d8ccea
Abel Majadibodu <Abeljackson33@gmail.com>
2023-08-02 14:23:03 +0200

    Switched to world time api and from axios to fetch

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 interface TimeProps {
   timeZone: string;
 }
@@ -9,10 +8,10 @@ const Time: React.FC<TimeProps> = ({ timeZone }) => {
   useEffect(() => {
     const fetchCurrentTime = async () => {
       try {
-        const response = await axios.get(
-          `https://www.timeapi.io/api/Time/current/zone?timeZone=${timeZone}`,
-        );
-        setCurrentTime(response.data);
+        const response = await fetch(
+          `http://worldtimeapi.org/api/timezone/${timeZone}`);
+        const data = await response.json();
+        setCurrentTime(data);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching current time:", error);
@@ -29,19 +28,12 @@ const Time: React.FC<TimeProps> = ({ timeZone }) => {
       ) : (
         currentTime && (
           <div>
-            <p>Year: {currentTime.year}</p>
-            <p>Month: {currentTime.month}</p>
-            <p>Day: {currentTime.day}</p>
-            <p>Hour: {currentTime.hour}</p>
-            <p>Minute: {currentTime.minute}</p>
-            <p>Seconds: {currentTime.seconds}</p>
-            <p>MilliSeconds: {currentTime.milliSeconds}</p>
-            <p>DateTime: {currentTime.dateTime}</p>
-            <p>Date: {currentTime.date}</p>
-            <p>Time: {currentTime.time}</p>
-            <p>TimeZone: {currentTime.timeZone}</p>
-            <p>Day of Week: {currentTime.dayOfWeek}</p>
-            <p>DST Active: {currentTime.dstActive ? "Yes" : "No"}</p>
+            {/* TODO: Display all time data and format properly */}
+            <p>Current time: {currentTime.datetime}</p>
+            <p>Time zone: {currentTime.timezone}</p>
+            <p>Day of week: {currentTime.day_of_week}</p>
+            <p>Day of year: {currentTime.day_of_year}</p>
+            <p>UTC offset: {currentTime.utc_offset}</p>
           </div>
         )
       )}
--- src/components/TimeZone.tsx
+++ src/components/TimeZone.tsx
@@ -1,69 +1,75 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Time from "./Time";
-interface TimeZoneProps {}
-const TimeZone: React.FC<TimeZoneProps> = ({}) => {
-  const [timeZones, setTimeZones] = useState<string[]>([]);
-  const [timeZone, setTimeZone] = useState("");
-  const [filteredTimeZones, setFilteredTimeZones] = useState<string[]>([]);
-  const [showAllTimeZones, setShowAllTimeZones] = useState(true);
-  const [loading, setLoading] = useState(true);
-  const [selectedTimeZone, setSelectedTimeZone] = useState("");
-  useEffect(() => {
+import React, { useState, useEffect } from "react"; 
+import Time from "./Time"; 
+ 
+interface TimeZoneProps {} 
+ 
+const TimeZone: React.FC<TimeZoneProps> = ({}) => { 
+  const [timeZones, setTimeZones] = useState<string[]>([]); 
+  const [filteredTimeZones, setFilteredTimeZones] = useState<string[]>([]); 
+  const [showAllTimeZones, setShowAllTimeZones] = useState(true); 
+  const [loading, setLoading] = useState(true); 
+  const [selectedTimeZone, setSelectedTimeZone] = useState(""); 
+ 
+  useEffect(() => { 
     const fetchTimeZones = async () => {
       try {
-        const response = await axios.get(
-          "https://www.timeapi.io/api/TimeZone/AvailableTimeZones",
+        const response = await fetch(
+          "http://worldtimeapi.org/api/timezone",
         );
-        setTimeZones(response.data);
-        setFilteredTimeZones(response.data);
+        const data = await response.json();
+        setTimeZones(data);
+        setFilteredTimeZones(data);
       } catch (error) {
         console.error("Error fetching time zones:", error);
       } finally {
         setLoading(false);
       }
     };
-    fetchTimeZones();
-  }, []);
-  const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const filterValue = event.target.value.toLowerCase();
-    const filtered = timeZones.filter((timezone) =>
-      timezone.toLowerCase().includes(filterValue),
-    );
-    setFilteredTimeZones(filtered);
-  };
-  const handleTimeZoneClick = (timezone: string) => {
-    setSelectedTimeZone(timezone);
-  };
-  return (
-    <>
-      <div>
-        <input
-          type="text"
-          placeholder="Filter time zones"
-          onChange={handleFilter}
-        />
-        <button onClick={() => setShowAllTimeZones(!showAllTimeZones)}>
-          {showAllTimeZones ? "Hide" : "Show"}
-        </button>
-        {loading ? (
-          <p>Loading...</p>
-        ) : showAllTimeZones ? (
-          <ul>
-            {filteredTimeZones.map((timezone) => (
-              <li
-                key={timezone}
-                onClick={() => handleTimeZoneClick(timezone)}
-                className={selectedTimeZone === timezone ? "active" : ""}
-              >
-                {timezone}
-              </li>
-            ))}
-          </ul>
-        ) : null}
-      </div>
-      {selectedTimeZone && <Time timeZone={selectedTimeZone} />}
-    </>
-  );
-};
+    fetchTimeZones(); 
+  }, []); 
+ 
+  const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => { 
+    const filterValue = event.target.value.toLowerCase(); 
+    const filtered = timeZones.filter((timezone) => 
+      timezone.toLowerCase().includes(filterValue) 
+    ); 
+    setFilteredTimeZones(filtered); 
+  }; 
+ 
+  const handleTimeZoneClick = (timezone: string) => { 
+    setSelectedTimeZone(timezone); 
+  }; 
+ 
+  return ( 
+    <> 
+      <div> 
+        <input 
+          type="text" 
+          placeholder="Filter time zones" 
+          onChange={handleFilter} 
+        /> 
+        <button onClick={() => setShowAllTimeZones(!showAllTimeZones)}> 
+          {showAllTimeZones ? "Hide" : "Show"} 
+        </button> 
+        {loading ? ( 
+          <p>Loading...</p> 
+        ) : showAllTimeZones ? ( 
+          <ul> 
+            {filteredTimeZones.map((timezone) => ( 
+              <li 
+                key={timezone} 
+                onClick={() => handleTimeZoneClick(timezone)} 
+                className={selectedTimeZone === timezone ? "active" : ""} 
+              > 
+                {timezone} 
+              </li> 
+            ))} 
+          </ul> 
+        ) : null} 
+      </div> 
+      {selectedTimeZone && <Time timeZone={selectedTimeZone} />} 
+    </> 
+  ); 
+}; 
+ 
 export default TimeZone;
